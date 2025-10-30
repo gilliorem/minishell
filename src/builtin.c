a15f04fe5d250e186ed765fdefdfa79ed7e48883
gilliorem <remi.gilliot98@gmail.com>
2025-10-31 06:41:26 +0800

    fck, I found my bug,I was comparing the wrong value, I should compare the key not the value because I am updating the value so it REMI=, thats the part I am converting.

--- src/builtin.c
+++ src/builtin.c
@@ -65,26 +65,57 @@ int	cd_builtin(char *arg, char *path, t_shell *shell)
 //if the variable already exist, I need to overwrite it instead of adding a new one.
 //need to upgrade the logic and actually check for the value of var
 //in the case of overwriting it.
+
+
+// char *var = REMI=BG
+// var_values[0] = REMI
+// var_values[1] = BG <<< overwrite this one.
+// we find the var value.
+// we compare it with the other var value
+// or we can check if it is in env_list[i]
+// if it is, we just re write the whole line.
+//
+//
+//
+// I need to have a list of the keys and a list the values
+// for the env variables. it is going to make things so much simpler
+// TODO: make a list of the env keys and the envs values (two separate lists)
+// meanwhile, I will perform a split.
 int	export_builtin(t_shell *shell, char *arg, char *var)
 {
 	int	i;
+	char **var_values;
+	char **env_values;
 
 	i = 0;
 	if (ft_strncmp(arg, "export", 7) != 0)
 	{
 		return (0);
-	}
+	} 
+	var_values= ft_split(var, '=');
+	
 	while (shell->env_list[i])
 	{
-		if (ft_strnstr(shell->env_list[i], var, ft_strlen(var)) != NULL)
+		//printf("%s\n", shell->env_list[i]);
+		env_values = ft_split(shell->env_list[i], '=');
+		printf("var_v0:[%s], var_v1:[%s]\n", var_values[0], var_values[1]);
+		printf("env0:[%s], env1:[%s]\n", env_values[0], env_values[1]);
+		/*
+		if (ft_strncmp(var_values[1], env_values[1], ft_strlen(env_values[1])) == 0)
 		{
+			printf("var_value[1]:[%s], env_values[1]:[%s]\n",var_values[1], env_values[1]);
+			printf("%s\n", shell->env_list[i]);
+			free(shell->env_list[i]);
+			shell->env_list[i] = NULL;
 			shell->env_list[i] = ft_strdup(var);
-			printf("var match with a current var\n");
+			return (1);
 		}
+		*/
 		i++;
 	}
 	if (shell->env_list[i] == NULL)
 	{
+		printf("going in the NULL if\n");
 		shell->env_list[i] = ft_strdup(var);
 		i++;
 		shell->env_list[i] = NULL;
